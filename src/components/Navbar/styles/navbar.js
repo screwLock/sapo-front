b0a5c8de8c1145ce29193695b36bd2a2e04c53c7
screwLock <helmlyw@gmail.com>
2018-10-08 17:44:08 -0400

    Sidebar link now colored on select

--- src/components/Navbar/styles/navbar.js
+++ src/components/Navbar/styles/navbar.js
@@ -4,36 +4,44 @@ import { NavLink } from 'react-router-dom';
 
 
 export const StyledNavbar = styled.div`
-    display: flex;
 
     ul {
         list-style: none;
         padding: 0;
         left-margin: 0;
+        display: flex;
+        flex-direction: column;
+
 
         li {
             top-margin: 0;
             font-size: 16px;
-            padding: 15px;
-            justify-content: center;
-            align-items: center;
-
+            display: flex;
 
             &:hover {
                 background: #f4f4f4;
                 color:#0078d4;
             }
+
         }
     }
 `;
 
 export const StyledNavLink = styled(NavLink)`
     color: black;
+    padding: 15px;
+    flex: 1;
     &.selected{
         font-weight: bold;
         color:#0078d4;
+        background-color: #f4f4f4;
+
+        :hover {
+            background-color: #DCDCDC;
+        }
     }
     &:hover {
         text-decoration:none;
     }
+
 `;
