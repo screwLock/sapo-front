c36419a3ae175ed354f620ddac325c28788e9d95
screwLock <helmlyw@gmail.com>
2019-02-04 08:10:04 -0500

    Added Terms of Service

--- src/components/Authenticator/SignUp.js
+++ src/components/Authenticator/SignUp.js
@@ -1,97 +1,108 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { Button, Classes, FormGroup, InputGroup, Intent, Dialog } from "@blueprintjs/core"
+import { AnchorButton, Button, Checkbox, Classes, FormGroup, InputGroup, Intent, Dialog } from "@blueprintjs/core"
 import { Auth } from "aws-amplify"
 import { Redirect, withRouter } from "react-router-dom"
 import * as EmailValidator from 'email-validator'
+import Terms from './Terms'
 
 class SignUp extends React.Component {
     static defaultProps = {
-      authData: {},
-      authState: false,
-      onAuthStateChange: (next, data) => { console.log(`SignUp:onAuthStateChange(${next}, ${JSON.stringify(data, null, 2)})`); }
+        authData: {},
+        authState: false,
+        onAuthStateChange: (next, data) => { console.log(`SignUp:onAuthStateChange(${next}, ${JSON.stringify(data, null, 2)})`); }
     };
-   
+
     constructor(props) {
-      super(props);
-      this.state = {
-        authData: this.props.authData,
-        authState: this.props.authState,
-        modalShowing: false,
-        error: null,
-        loading: false,
-        successShowing: false,
-        firstName: '',
-        lastName: '',
-        organization: '',
-        email: '',
-        phone: '',
-        password: '',
-        confirmPassword: '',
-        access: 'admin'
-      };
+        super(props);
+        this.state = {
+            authData: this.props.authData,
+            authState: this.props.authState,
+            modalShowing: false,
+            error: null,
+            loading: false,
+            successShowing: false,
+            firstName: '',
+            lastName: '',
+            organization: '',
+            email: '',
+            phone: '',
+            password: '',
+            confirmPassword: '',
+            access: 'admin',
+            isChecked: false,
+            isTermsOpen: false,
+        };
     }
 
     handleBack = () => {
         this.props.history.push('/')
     }
 
+    handleCheckedChange = (e) => {
+        this.setState({isChecked: !this.state.isChecked})
+    }
+
     handleChange = e => this.setState({ [e.target.name]: e.target.value })
 
+    handleTermsClose = () => this.setState({ isTermsOpen: false})
+
+    handleTermsOpen = () => this.setState({ isTermsOpen: true})
+
     handleSignUp = () => {
-        if(this.validateForms()){
+        if (this.validateForms()) {
             this.onSignUp()
         }
         else {
             return false
         }
     }
-   
+
     onSignUp = async () => {
-      try {
-        this.setState({ loading: true });
-        const response = await Auth.signUp({
-          username: this.state.email, 
-          password: this.state.password, 
-           attributes: {
-            'custom:first_name': this.state.firstName,
-            'custom:last_name': this.state.lastName, 
-            'custom:phone': this.state.phone,
-            'custom:organization': this.state.organization,
-            'custom:access': this.state.access,
-          }
-        });
-        console.log(`SignUp::onSignUp(): Response#1 = ${JSON.stringify(response, null, 2)}`);
-        if (response.userConfirmed === false) {
-          this.setState({ authData: response, modalShowing: true, loading: false, successShowing: true });
-        } else {
-          this.onAuthStateChange('default', { username: response.username });
+        try {
+            this.setState({ loading: true });
+            const response = await Auth.signUp({
+                username: this.state.email,
+                password: this.state.password,
+                attributes: {
+                    'custom:first_name': this.state.firstName,
+                    'custom:last_name': this.state.lastName,
+                    'custom:phone': this.state.phone,
+                    'custom:organization': this.state.organization,
+                    'custom:access': this.state.access,
+                }
+            });
+            console.log(`SignUp::onSignUp(): Response#1 = ${JSON.stringify(response, null, 2)}`);
+            if (response.userConfirmed === false) {
+                this.setState({ authData: response, modalShowing: true, loading: false, successShowing: true });
+            } else {
+                this.onAuthStateChange('default', { username: response.username });
+            }
+        } catch (err) {
+            console.log(`SignUp::onSignUp(): Error ${JSON.stringify(err, null, 2)}`);
+            this.setState({ error: err.message, loading: false });
         }
-      } catch (err) {
-        console.log(`SignUp::onSignUp(): Error ${JSON.stringify(err, null, 2)}`);
-        this.setState({ error: err.message, loading: false });
-      }
     }
-   
+
     onConfirmSubmitted = async (token) => {
-      try {
-        this.setState({ loading: true });
-        const response = await Auth.confirmSignUp(this.state.email, token);
-        console.log(`SignUp::onConfirmSubmitted(): Response#2 = ${JSON.stringify(response, null, 2)}`);
-        this.setState({ loading: false });
-        if (response === 'SUCCESS') {
-          this.props.onAuthStateChange('default', { email: this.state.email });
+        try {
+            this.setState({ loading: true });
+            const response = await Auth.confirmSignUp(this.state.email, token);
+            console.log(`SignUp::onConfirmSubmitted(): Response#2 = ${JSON.stringify(response, null, 2)}`);
+            this.setState({ loading: false });
+            if (response === 'SUCCESS') {
+                this.props.onAuthStateChange('default', { email: this.state.email });
+            }
+        } catch (err) {
+            console.log(`SignUp::onConfirmSubmitted(): Error ${JSON.stringify(err, null, 2)}`);
+            this.setState({ error: err.message, loading: false });
         }
-      } catch (err) {
-        console.log(`SignUp::onConfirmSubmitted(): Error ${JSON.stringify(err, null, 2)}`);
-        this.setState({ error: err.message, loading: false });
-      }
     }
 
     validateForms = () => {
         const phoneValidate = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
         const passwordTest = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-        if (this.state.email.length === 0 || 
+        if (this.state.email.length === 0 ||
             this.state.password.length === 0 ||
             this.state.confirmPassword.length === 0 ||
             this.state.firstName.length === 0 ||
@@ -99,23 +110,27 @@ class SignUp extends React.Component {
             this.state.phone.length === 0 ||
             this.state.organization.length === 0
         ) {
-            this.setState({error: 'Required fields are missing'})
+            this.setState({ error: 'Required fields are missing' })
             return false
         }
-        else if(!EmailValidator.validate(this.state.email)){
-            this.setState({error: 'Enter a valid email address'})
+        else if (!EmailValidator.validate(this.state.email)) {
+            this.setState({ error: 'Enter a valid email address' })
             return false
         }
-        else if(!phoneValidate.test(this.state.phone)){
-            this.setState({error: 'Enter a valid phone number'})
+        else if (!phoneValidate.test(this.state.phone)) {
+            this.setState({ error: 'Enter a valid phone number' })
             return false
         }
-        else if(this.state.password !== this.state.confirmPassword){
-            this.setState({error: 'Your passwords do not match'})
+        else if (this.state.password !== this.state.confirmPassword) {
+            this.setState({ error: 'Your passwords do not match' })
             return false
         }
-        else if(!passwordTest.test(this.state.password)){
-            this.setState({error: 'Your password does not meet the requirements'})
+        else if (!passwordTest.test(this.state.password)) {
+            this.setState({ error: 'Your password does not meet the requirements' })
+            return false
+        }
+        else if (!this.state.isChecked) {
+            this.setState({ error: 'You must agree to the terms of service' })
             return false
         }
         else {
@@ -125,7 +140,7 @@ class SignUp extends React.Component {
 
 
     showSuccessMessage = () => {
-        if(this.state.successShowing){
+        if (this.state.successShowing) {
             return 'Sign up successful.  Please check your email and click the confirmation link to login.';
         }
     }
@@ -134,73 +149,79 @@ class SignUp extends React.Component {
         const errorComponent = this.state.error !== null
             ? this.state.error
             : '';
-        
+
 
         return (
-            <Container>
-                {/*this.state.error !== null && errorComponent*/}
-                <FormContainer>
-                    <Logo>SAPO</Logo>
-                    <FormGroup
-                        label="First Name"
-                        labelFor="text-input"
-                    >
-                        <InputGroup name="firstName" onChange={this.handleChange}/>
-                    </FormGroup>
-                    <FormGroup
-                        label="Last Name"
-                        labelFor="text-input"
-                    >
-                        <InputGroup name="lastName" onChange={this.handleChange}/>
-                    </FormGroup>
-                    <FormGroup
-                        label="Email"
-                        labelFor="text-input"
-                    >
-                        <InputGroup name="email" onChange={this.handleChange}/>
-                    </FormGroup>
-                    <FormGroup
-                        label="Organization"
-                        labelFor="text-input"
-                    >
-                        <InputGroup name="organization" onChange={this.handleChange}/>
-                    </FormGroup>
-                    <FormGroup
-                        label="Phone"
-                        labelFor="text-input"
-                    >
-                        <InputGroup name="phone" onChange={this.handleChange}/>
-                    </FormGroup>
-                    <FormGroup
-                        label="Password"
-                        labelFor="text-input"
-                        helperText="Passwords need to be a minimum of 8 characters, contain 1 special character, 1 upper case letter, 1 lower case letter, and 1 number"
-                    >
-                        <InputGroup name="password" onChange={this.handleChange} type='password'/>
-                    </FormGroup>
-                    <FormGroup
-                        label="Confirm Password"
-                        labelFor="text-input"
-                    >
-                        <InputGroup name="confirmPassword" onChange={this.handleChange} type='password'/>
-                    </FormGroup>
-                    <TermsContainer>I accept the SAPO terms of service.</TermsContainer>
-                    <ButtonRow>
-                        <Button
-                            loading={this.state.loading}
-                            onClick={this.handleSignUp}
-                            text='Sign Up'
-                        />
-                        <Button
-                            loading={this.state.loading}
-                            onClick={this.handleBack}
-                            text='Back'
-                        />
-                    </ButtonRow>
-                    <ErrorContainer>{`${errorComponent}`}</ErrorContainer>
-                    <SuccessContainer>{this.showSuccessMessage()}</SuccessContainer>
-                </FormContainer>
-            </Container>
+            <div>
+                <Container>
+                    {/*this.state.error !== null && errorComponent*/}
+                    <FormContainer>
+                        <Logo>SAPO</Logo>
+                        <FormGroup
+                            label="First Name"
+                            labelFor="text-input"
+                        >
+                            <InputGroup name="firstName" onChange={this.handleChange} />
+                        </FormGroup>
+                        <FormGroup
+                            label="Last Name"
+                            labelFor="text-input"
+                        >
+                            <InputGroup name="lastName" onChange={this.handleChange} />
+                        </FormGroup>
+                        <FormGroup
+                            label="Email"
+                            labelFor="text-input"
+                        >
+                            <InputGroup name="email" onChange={this.handleChange} />
+                        </FormGroup>
+                        <FormGroup
+                            label="Organization"
+                            labelFor="text-input"
+                        >
+                            <InputGroup name="organization" onChange={this.handleChange} />
+                        </FormGroup>
+                        <FormGroup
+                            label="Phone"
+                            labelFor="text-input"
+                        >
+                            <InputGroup name="phone" onChange={this.handleChange} />
+                        </FormGroup>
+                        <FormGroup
+                            label="Password"
+                            labelFor="text-input"
+                            helperText="Passwords need to be a minimum of 8 characters, contain 1 special character, 1 upper case letter, 1 lower case letter, and 1 number"
+                        >
+                            <InputGroup name="password" onChange={this.handleChange} type='password' />
+                        </FormGroup>
+                        <FormGroup
+                            label="Confirm Password"
+                            labelFor="text-input"
+                        >
+                            <InputGroup name="confirmPassword" onChange={this.handleChange} type='password' />
+                        </FormGroup>
+                        <TermsContainer>I accept the 
+                            <a onClick={this.handleTermsOpen}> SAPO terms of service.</a>
+                            <TermsCheckbox checked={this.state.isChecked} inline={true} onChange={this.handleCheckedChange} />
+                        </TermsContainer>
+                        <ButtonRow>
+                            <Button
+                                loading={this.state.loading}
+                                onClick={this.handleSignUp}
+                                text='Sign Up'
+                            />
+                            <Button
+                                loading={this.state.loading}
+                                onClick={this.handleBack}
+                                text='Back'
+                            />
+                        </ButtonRow>
+                        <ErrorContainer>{`${errorComponent}`}</ErrorContainer>
+                        <SuccessContainer>{this.showSuccessMessage()}</SuccessContainer>
+                    </FormContainer>
+                </Container>
+                <Terms onClose={this.handleTermsClose} isOpen={this.state.isTermsOpen}/>
+            </div>
         );
     }
 }
@@ -218,6 +239,7 @@ const FormContainer = styled.div`
     display: flex;
     flex-direction: column;
     width: 350px;
+    margin-top: 150px;
 `;
 
 const Logo = styled.div`
@@ -245,5 +267,9 @@ const SuccessContainer = styled.p`
 const TermsContainer = styled.div`
     margin-top: 10px;
 `;
+
+const TermsCheckbox = styled(Checkbox)`
+    margin-left: 10px;
+`
 
 export default withRouter(SignUp);
