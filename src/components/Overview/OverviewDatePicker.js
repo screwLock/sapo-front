2dac109b8b20ca28490ec3f68425b27dc18db0ed
screwLock <helmlyw@gmail.com>
2019-03-10 04:38:44 -0400

    Unconfirmed dates pulse in the datepicker

--- src/components/Overview/OverviewDatePicker.js
+++ src/components/Overview/OverviewDatePicker.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import { keyframes } from "styled-components";
 
 class OverviewDatePicker extends Component {
   constructor(props) {
@@ -22,15 +23,33 @@ class OverviewDatePicker extends Component {
   render() {
     // highlighted should be same month as selectedDate
     // as well as !confirmed
-    const modifiers = { highlighted: {
-      from: new Date(2019, this.props.selectedMonth, 12), 
-      to: new Date(2019, this.props.selectedMonth, 16) 
+    const modifiers = {
+      highlighted: {
+        from: new Date(2019, this.props.selectedMonth, 12),
+        to: new Date(2019, this.props.selectedMonth, 16)
+      }
     }
-    }
+
+    const pulse = keyframes `
+      0% {
+        color: red;
+      }
+      50% {
+        color: black;
+      }
+      100 {
+        color: red;
+      }
+    `
+
     const highlightedStyle = `.DayPicker-Day--highlighted {
-      background-color: red;
-      color: white;
-    }`;
+      color: red;
+      font-weight: 800;
+      animation-name: ${pulse};
+      animation-duration: 2s;
+      animation-iteration-count: infinite;
+    }`
+    
     return (
       <div>
         <style>{highlightedStyle}</style>
