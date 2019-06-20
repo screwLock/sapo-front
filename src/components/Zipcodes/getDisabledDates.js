0fbbdac5ae8b9bd3120aa3af6942a1ae2bd30a54
screwLock <helmlyw@gmail.com>
2019-06-20 09:47:32 -0400

    zipcodes fixed

--- src/components/Zipcodes/getDisabledDates.js
+++ src/components/Zipcodes/getDisabledDates.js
@@ -41,25 +41,26 @@ const getWeekdayValues = (weekdays) => {
     return weekdays.map((day) => Object.keys(day).filter((k) => day[k] && k !== 'day'))
 }
 
+
 const convertOrdinalsToNumbers = (ordinals) => {
     const dayNumbers = [];
     if (ordinals.includes('all')) {
         dayNumbers.push(1, 2, 3, 4, 5)
         return dayNumbers;
     }
-    else if (ordinals.includes('first')) {
+    if (ordinals.includes('first')) {
         dayNumbers.push(1)
     }
-    else if (ordinals.includes('second')) {
+    if (ordinals.includes('second')) {
         dayNumbers.push(2)
     }
-    else if (ordinals.includes('third')) {
+    if (ordinals.includes('third')) {
         dayNumbers.push(3)
     }
-    else if (ordinals.includes('fourth')) {
+    if (ordinals.includes('fourth')) {
         dayNumbers.push(4)
     }
-    else if (ordinals.includes('fifth')) {
+    if (ordinals.includes('fifth')) {
         dayNumbers.push(5)
     }
     return dayNumbers;
