aa1ad2ada5036bae254d94dd1d303a7300ce7d35
screwLock <helmlyw@gmail.com>
2020-09-10 23:56:31 -0400

    Added all pickup info to PickupInfo

--- src/components/Overview/OverviewPickupsView/PickupInfo.js
+++ src/components/Overview/OverviewPickupsView/PickupInfo.js
@@ -41,6 +41,26 @@ class PickupInfo extends React.Component {
                 <h3>{pickup.streetAddress}, {pickup.zipcode}</h3>
                 <h5>{pickup.phoneNumber}</h5>
                 <h5>{pickup.email}</h5>
+                <h5>This pickup has been {pickup.status.toUpperCase()}</h5>
+                <div>Order:
+                    {/* this is for early pickups when donations was an array instead of an object} */}
+                    {/* from here on out, donations is an object with key-value pairs of name: qty */}
+                    {/* should probably delete ALL pickups before 7/8/19 from dynamoDB */}
+                    {Array.isArray(pickup.donations) ? (
+                        <ul>{pickup.donations.map(donation => (<li key={donation}>{donation}</li>))}</ul>
+                    ) : (<ul>{Object.entries(pickup.donations).map((kv) => {
+                        return (<li key={kv[0]}>{`${kv[0]} - Qty: ${kv[1]}`}</li>)
+                    })} </ul>
+                        )}
+                </div>
+                <div>Pickup Details:
+                            <ul>{pickup.serviceDetails.map(detail => {
+                    return (<li key={detail}>{`${detail}`}</li>)
+                })
+                    }</ul>
+                </div>
+                <div>Comments: {pickup.comments ? `${pickup.comments}` : 'None'}</div>
+                <div>Submitted By: {pickup.createdBy}</div>
             </div>
         )
     }
