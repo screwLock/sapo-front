0ae6e1853adc200be52d6b8017ed58b656032ee0
screwLock <helmlyw@gmail.com>
2020-09-24 01:55:48 -0400

    Removed transform to fix drag and drop

--- src/components/Overview/OverviewViewHandler.js
+++ src/components/Overview/OverviewViewHandler.js
@@ -28,9 +28,9 @@ class OverviewViewHandler extends React.Component {
         return (
             <Transition
                 items={this.state.view}
-                from={{ opacity: 0, transform: 'translate3d(100%,0,0)', }}
-                enter={{ opacity: 1, transform: 'translate3d(0%,0,0)',  }}
-                leave={{ opacity: 0, transform: 'translate3d(-100%,0,0)'}}
+                from={{ opacity: 0,  }}
+                enter={{ opacity: 1,  }}
+                leave={{ opacity: 0,}}
                 native
                 reset
                 unique
