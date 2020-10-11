2be84c5c8edab64e7943b3dfc9771bb647a4cc1c
screwLock <helmlyw@gmail.com>
2020-10-11 16:53:57 -0400

    filename change

--- src/components/Account/Branding.js
+++ src/components/Account/Branding.js
@@ -34,7 +34,7 @@ const Branding = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
-        const filename = `/branding/${selectedLogo.name}`
+        const filename = `/branding/logo`
         try {
             const stored = await Storage.put(filename, selectedLogo, {
                 contentType: selectedLogo.type,
