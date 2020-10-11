0c3710cd166bc229f0ec74221984e217c9b0735c
screwLock <helmlyw@gmail.com>
2020-10-10 21:03:54 -0400

    signature pad is working

--- src/components/Emails/Receipts.js
+++ src/components/Emails/Receipts.js
@@ -1,44 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import styled from 'styled-components'
 import { Button, FileInput, FormGroup, H6, InputGroup, Intent } from "@blueprintjs/core"
 import { Storage } from "aws-amplify";
+import SignaturePad from 'react-signature-canvas'
 
 const Receipts = (props) => {
 
     const [text, setText] = useState('Choose PNG or JPEG/JPG...')
     const [errorText, setErrorText] = useState('')
-    const [selectedLogo, setSelectedLogo] = useState(null)
+    const [trimmedDataURL, setTrimmedDataURL] = useState(null)
     const [loading, setLoading] = useState(false)
 
-    // On file select (from the pop up) 
-    const onFileChange = event => {
-        let logo = event.target.files[0]
-        // Image upload was canceled
-        if (!logo) {
-            return
-        }
-        if (logo.size > 1000000) {
-            setErrorText('Image should not exceed a megabyte')
-            return
-        }
-        else if (logo.type !== 'image/png' && logo.type !== 'image/jpeg' && logo.type !== 'image/jpg') {
-            setErrorText('Image should be a PNG or a JPEG/JPG')
-            return
-        }
-        // Update the state 
-        setErrorText('')
-        setSelectedLogo(logo)
-
-    };
+    const sigPad = useRef({});
+    const clear = () => {
+      sigPad.current.clear()
+    }
+    const save = () => {
+      setTrimmedDataURL(sigPad.getTrimmedCanvas()
+        .toDataURL('image/png'))
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
-        const filename = selectedLogo.name
         try {
-            const stored = await Storage.vault.put(filename, selectedLogo, {
-                contentType: selectedLogo.type,
-              });
             // key is located in stored.key if it's needed
             setLoading(false)
         } catch (e) {
@@ -52,23 +37,18 @@ const Receipts = (props) => {
 
     return (
         <>
-            <H6>Upload Your Logo and Header</H6>
-            <H6>We recommend using a logo with a maximum height of 100px</H6>
-            <StyledInput text={text} onInputChange={onFileChange} />
-            <FileName>{selectedLogo ? selectedLogo.name : ''}</FileName>
-            <Button disabled={!selectedLogo} loading={loading} text='Submit' onClick={handleSubmit} />
+            <H6>Create a Signature</H6>
+            <div>
+                <SignaturePad 
+                    canvasProps={{width: 500, height: 200}}
+                    ref={sigPad} 
+                />
+            </div>
+            <Button disabled={true} loading={loading} text='Submit' onClick={handleSubmit} />
             <ErrorText>{errorText}</ErrorText>
         </>
     )
 }
-
-const StyledInput = styled(FileInput)`
-    margin: 1.5em;
-`
-
-const FileName = styled.div`
-    color: blue;
-`
 
 const ErrorText = styled.div`
     color: red;
